13ad91d85ea706806b828fafb36370ca749ab6e8
oribe <oribenmoha7@gmail.com>
2026-05-24 20:13:53 +0300

    commit 46

--- api/routes/songRouter.js
+++ api/routes/songRouter.js
@@ -1,21 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const {addReviewToSong, getSongReviews, createSong ,getAllSongs ,getSongById ,getSongsByUser,deleteSong ,updateRating,updateSong} = require("../controllers/songController");
+const {addReviewToSong, getSongReviews, createSong ,getAllSongs ,getSongById} = require("../controllers/songController");
 
 const auth = require("../middleware/auth");
 
 // CRUD + דירוג
 // ציבורי
 router.get("/", getAllSongs);
-router.get("/user/:firebase_id_ref", getSongsByUser);
 router.get("/:id/reviews", getSongReviews);
 router.get("/:id", getSongById);
 router.post("/:id/review", addReviewToSong);
 
 // מוגן
 router.post("/", auth, createSong);
-router.patch("/:id", auth, updateSong);
-router.patch("/:id/rating", auth, updateRating);
-router.delete("/:id", auth, deleteSong);
 
 module.exports = router;
